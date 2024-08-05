eb59e6825ec366b7b38df5e9cbabec1f672abfd0
SonnyFishback <sonny.fishback7@gmail.com>
2024-08-04 23:05:46 -0400

    added null check to prevent addEventListener on undefined error

--- client/src/javascript/index.js
+++ client/src/javascript/index.js
@@ -9,40 +9,10 @@ const hamMenu = document.getElementById("ham-menu");
 
 const offScreenMenu = document.getElementById("off-screen-menu");
 
-hamMenu.addEventListener("click", () => {
-  hamMenu.classList.toggle("active");
-  offScreenMenu.classList.toggle("active");
-});
-
-
-
-
-
-
-
-
-
-
-
-
-// removing autoplay on video with Observer
-
-// const videoPlayer = document.getElementById("player");
-
-// const observer = new IntersectionObserver(
-//   (entries) => {
-//     entries.forEach((entry) => {
-//       if (entry.isIntersecting) {
-//         videoPlayer.play();
-//       } else {
-//         videoPlayer.pause();
-//       }
-//     });
-//   },
-//   {
-//     root: null, 
-//     rootMargin: "0px", 
-//     threshold: 0.5, 
-//   }
-// );
-// observer.observe(videoPlayer);
+if ( hamMenu ) {
+  hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+    offScreenMenu.classList.toggle("active");
+  });
+  
+}
