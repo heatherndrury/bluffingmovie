923d0fa1f3fd2559b8f6b615105322cb6fdce1bb
SonnyFishback <sonny.fishback7@gmail.com>
2024-08-04 23:12:16 -0400

    clean up

--- client/src/javascript/index.js
+++ client/src/javascript/index.js
@@ -9,7 +9,6 @@ const hamMenu = document.getElementById("ham-menu");
 
 const offScreenMenu = document.getElementById("off-screen-menu");
 
-
 hamMenu?.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
